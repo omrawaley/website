d2f4e13fc282d470284e90caf8f8ba84d470872a
omrawaley <omrawaley@icloud.com>
2026-06-04 13:05:02 -0400

    feat: add empty pages for contact/ and about/ sections

---
+++ app/about/page.tsx
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: "About",
+  description: "Learn more about me.",
+};
+
+export default function Page() {
+  return (
+    <section>
+      <h1 className="mb-4 text-2xl font-bold tracking-tighter">About</h1>
+    </section>
+  );
+}
---
+++ app/contact/page.tsx
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: "Contact",
+  description: "Reach out to me.",
+};
+
+export default function Page() {
+  return (
+    <section>
+      <h1 className="mb-4 text-2xl font-bold tracking-tighter">Contact</h1>
+    </section>
+  );
+}
