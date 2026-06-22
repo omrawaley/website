bae907ecbd9432c9a1118c5b4f91b5b5f948a258
omrawaley <omrawaley@icloud.com>
2026-06-22 15:15:17 -0400

    feat: add contact link placeholders

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -7,6 +7,10 @@ export default function Page() {
   return (
     <section>
       <h1 className="mb-4 text-2xl font-bold tracking-tighter">Contact</h1>
+      <li className="flex flex-col">
+        <a className="cursor-pointer underline">GitHub</a>
+        <a className="cursor-pointer underline">Email</a>
+      </li>
     </section>
   );
 }
